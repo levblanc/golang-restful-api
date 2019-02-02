0a91416a9e4996db719270fb783a9346cb3246c7
Levblanc <levblanc@gmail.com>
2019-02-02 18:52:19 +0800

    chore: remove comment

--- utils/auth/session.go
+++ utils/auth/session.go
@@ -7,8 +7,6 @@ import (
 	"github.com/levblanc/golang-restful-api/db"
 )
 
-// db.session.createIndex({ "lastActivity": 1 }, { expireAfterSeconds: 10 })
-
 // Session defines data structure of a user session
 type Session struct {
 	Sid          string    `json:"sid" bson:"sid"`
